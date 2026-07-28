b5ba5883b9f24c44f7b02fdf748b83096a9cd2af
Krishnawasthi <km.awasthi.77@gmail.com>
2026-07-28 21:59:05 +0530

    supplier

--- functionalInterface/src/com/predicate/functional/Driver.java
+++ functionalInterface/src/com/predicate/functional/Driver.java
@@ -1,14 +1,19 @@
 package com.predicate.functional;
 
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Driver {
 public static void main(String[] args) {
 	
 	Predicate <Integer> predicate = (a) -> a < 100;
-	System.out.println(predicate.test(80));
-	System.out.println(predicate.test(143));
-
+	
+	System.out.println(predicate.test(80)); //true 
+	System.out.println(predicate.test(143)); //false
+	
+	
+	Supplier<String> supplier = () -> "Banglore"; 
+    System.out.println(supplier.get());
  }
 
 }
