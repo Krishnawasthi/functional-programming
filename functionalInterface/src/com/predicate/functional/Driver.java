08511660324ef467e73ae06e909fc274a11ab64f
Krishnawasthi <km.awasthi.77@gmail.com>
2026-07-28 22:12:07 +0530

    comsumer

--- functionalInterface/src/com/predicate/functional/Driver.java
+++ functionalInterface/src/com/predicate/functional/Driver.java
@@ -3,16 +3,19 @@ package com.predicate.functional;
 import java.util.Random;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 public class Driver {
 public static void main(String[] args) {
 	
+	
+	//predicate 
 	Predicate <Integer> predicate = (a) -> a < 100;
 	
 	System.out.println(predicate.test(80)); //true 
 	System.out.println(predicate.test(143)); //false
 	
-	
+	//suppplier
 	Supplier<String> supplier = () -> "Banglore"; 
     System.out.println(supplier.get());
     
@@ -20,6 +23,12 @@ public static void main(String[] args) {
     
     Supplier<Integer> supplier3 = () -> 100000 + new Random().nextInt(900000);
     System.out.println("Your otp is : " + supplier3.get());
-  }
+    
+    //consumer
+    
+    Consumer<String> consumer = (emailTo) -> System.out.println("Email has been sent : " + emailTo );
+      consumer.accept("kmawasthi.com");
+}
+
 
 }
